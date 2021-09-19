6abcf269176edddceaa69f3577790278bb0d58cf
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-19 22:49:26 +0300

    add a Review models

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.db.models.aggregates import Max
+from django.db.models.fields.related import ManyToManyField
 from accounts.models import Profile
 
 # Create your models here.
@@ -14,6 +16,7 @@ class Project(models.Model):
     date_posted = models.DateTimeField(auto_now_add=True)
 
 
+
     def save_project(self):
         return self.save()
 
@@ -22,3 +25,14 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+  design = models.CharField(max_length=2)
+  userbility = models.CharField(max_length=2)
+  content = models.CharField(max_length=2)
+  creativity = models.CharField(max_length=2)
+  project = ManyToManyField(Project)
+
+  def __str__(self):
+    return self.design
+
