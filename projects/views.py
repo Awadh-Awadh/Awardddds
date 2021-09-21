054341a1b55f1124e3eedc7681fba94f6850ecbb
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-21 16:57:41 +0300

    add project details template

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Review, Rating
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -9,10 +10,12 @@ def home(request):
     }
 
     return render(request,'projects/home.html',context)
-
+@login_required
 def projectDetail(request,pk):
     project = Project.objects.get(id = pk)
+    reviews = Review.objects.all()
     context = {
-      'project': project
-    }
+      'project': project,
+      'reviews':reviews
+          }
     return render(request, 'projects/detail.html', context)
